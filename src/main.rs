fa8bcdcb939692a4654f1684652dbfb31ad60512
7rah <54563510+7rah@users.noreply.github.com>
2021-06-21 17:58:52 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,73 @@
-use fast_socks5::client::Config;
-use fast_socks5::client::Socks5Stream;
-use log::error;
-use log::info;
-use log::LevelFilter;
-use smol::future;
-use smol::io;
-use smol::net::{TcpListener, TcpStream};
-use std::io::{Error, Result};
-use std::str::from_utf8;
+use anyhow::anyhow;
+use anyhow::Result;
+use std::env;
+use std::str;
 use tls_parser::{
     parse_tls_extensions, parse_tls_plaintext, TlsExtension, TlsMessage::Handshake,
     TlsMessageHandshake::ClientHello,
 };
+use tokio::io::copy;
+use tokio::io::split;
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpListener;
+use tokio::net::TcpStream;
+use tokio::spawn;
+use tokio_socks::tcp::Socks5Stream;
 
-fn new_error<T: ToString>(message: T) -> Error {
-    io::Error::new(io::ErrorKind::Other, message.to_string())
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+    let proxy = env::args().nth(1).unwrap_or_else(|| {
+        println!("Expect socks5 proxy addr,use default(127.0.0.1:1080)");
+        "127.0.0.1:1080".to_string()
+    });
+    let listener = TcpListener::bind("127.0.0.1:443").await.unwrap();
+
+    loop {
+        let proxy = proxy.clone();
+        let (inbound, _) = listener.accept().await?;
+
+        spawn(async move {
+            match serve(&proxy, inbound).await {
+                Ok(_) => {}
+                Err(e) => println!("Error: {}", e),
+            }
+        });
+    }
 }
 
-fn parse_tls_connection(buf: &[u8]) -> Result<String> {
-    let (_, res) = parse_tls_plaintext(&buf).map_err(|_| new_error("unexpected protocol"))?;
+async fn serve(proxy: &str, inbound: TcpStream) -> Result<()> {
+    let buf = &mut [0u8; 2048];
+    inbound.peek(buf).await?;
+    let domain = parse_sni(buf)?;
+    let outbound = Socks5Stream::connect(proxy, domain + ":443").await?;
+
+    let (mut ri, mut wi) = split(inbound);
+    let (mut ro, mut wo) = split(outbound);
+    let client_to_server = async {
+        copy(&mut ri, &mut wo).await?;
+        wo.shutdown().await
+    };
+
+    let server_to_client = async {
+        copy(&mut ro, &mut wi).await?;
+        wi.shutdown().await
+    };
+
+    tokio::try_join!(client_to_server, server_to_client)?;
+
+    Ok(())
+}
+
+fn parse_sni(buf: &[u8]) -> Result<String> {
+    let (_, res) = parse_tls_plaintext(&buf).map_err(|_| anyhow!("unexpected protocol"))?;
     match &res.msg[0] {
         Handshake(ClientHello(contents)) => {
             let ext = contents
                 .ext
-                .ok_or(())
-                .map_err(|_| new_error("unable to find tls extensions"))?;
+                .ok_or(anyhow!("unable to find tls extensions"))?;
 
-            let (_, exts) = parse_tls_extensions(ext)
-                .map_err(|_| new_error("unable to parse tls extensions"))?;
+            let (_, exts) =
+                parse_tls_extensions(ext).map_err(|_| anyhow!("unable to parse tls extensions"))?;
 
             let v = exts
                 .iter()
@@ -35,67 +75,11 @@ fn parse_tls_connection(buf: &[u8]) -> Result<String> {
                     TlsExtension::SNI(v) => Some(v),
                     _ => None,
                 })
-                .ok_or(())
-                .map_err(|_| new_error("unable to find tls extension SNI"))?;
+                .ok_or(anyhow!("unable to find tls extension SNI"))?;
 
-            let name = from_utf8(v[0].1).unwrap().to_string();
-            Ok(name)
+            let domain = str::from_utf8(v[0].1).unwrap().to_string();
+            Ok(domain)
         }
-        _ => Err(new_error("unexpected handshake type")),
+        _ => Err(anyhow!("unexpected handshake type")),
     }
-}
-
-async fn serve(stream: TcpStream) -> std::io::Result<()> {
-    let mut buf: [u8; 2048] = [0; 2048];
-    stream.peek(&mut buf).await?;
-
-    let domain = parse_tls_connection(&buf)?;
-    info!("accepted {}", domain);
-
-
-    let config = Config::default();
-    let socks = Socks5Stream::connect("192.168.1.1:1080", domain, 443, config)
-        .await
-        .map_err(new_error)?;
-
-    let (stream_reader, stream_writer) = io::split(stream);
-    let (socks_reader, socks_writer) = io::split(socks);
-
-    future::race(
-        async {
-            io::copy(stream_reader, socks_writer).await.map_err(|e| new_error(e.to_string()))
-        },
-        async {
-            io::copy(socks_reader, stream_writer).await.map_err(|e| new_error(e.to_string()))
-        },
-    )
-    .await?;
-
-    Ok(())
-}
-
-fn main() -> std::io::Result<()> {
-    smol::block_on(async {
-        let listener = TcpListener::bind("0.0.0.0:443").await?;
-        let _ = env_logger::builder()
-            .filter_module("sni2socks", LevelFilter::Info)
-            .try_init();
-
-        loop {
-            match listener.accept().await {
-                Ok((stream, _)) => smol::spawn(async {
-                    match serve(stream).await {
-                        Ok(_) => (),
-                        Err(e) => {
-                            error!("{}", e.to_string())
-                        }
-                    }
-                })
-                .detach(),
-                Err(e) => {
-                    error!("{}", e.to_string())
-                }
-            }
-        }
-    })
 }
